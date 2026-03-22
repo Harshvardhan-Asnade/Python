ad6fba68e8ab0998296b04854a237393db4b0b7b
Harshvardhan Asnade <harshasnade1@gmail.com>
2026-03-22 20:54:47 +0530

    oops/Constructor

---
+++ oops/Constructor.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self,name):
+        self.name=name
+    Subject="python"
+    College="Abc"
+    Year="2nd"
+
+st1=Student("Harsh")
+
+print(st1.name)
+print(st1.College)
+print(st1.Year)
+print(st1.Subject)
