98d0495c2c97bd42903912b2e6e3ebfdaf23efa0
Harshvardhan Asnade <harshasnade1@gmail.com>
2026-03-24 13:07:26 +0530

    Write in File

---
+++ File Handling/Write a File.py
@@ -0,0 +1,8 @@
+with open("sample.txt", "w") as f:
+    f.write("""Hello Harsh
+Welcome to Python File Handling
+
+This is a sample text file.
+You can read, write, and append data here.
+
+Keep learning and practicing!""")
