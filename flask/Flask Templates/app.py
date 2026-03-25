3511968f07101eb7ef3fbaaf0dfe5aba6a479060
Harshvardhan Asnade <harshasnade1@gmail.com>
2026-03-25 22:30:44 +0530

    Make First Website Using Html using Flask

--- flask/Flask Templates/app.py
+++ flask/Flask Templates/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+app=Flask(__name__)
 
-app = Flask(__name__)
-
-
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 @app.route("/")
 def home():
-    return render_template("index.html", name="Harsh")
-
-app.run(debug=True, port=5001)
+    return render_template("index.html",name="Harsh")
+@app.route('/about')
+def about():
+    return render_template("about.html")
+if __name__=="__main__":
+    app.run(debug=True)
